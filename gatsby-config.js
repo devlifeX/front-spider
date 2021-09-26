4f28c81b832e4626ca9154466c93a14770abff4a
dariushvesal <dariush.vesal@gmail.com>
2021-09-26 22:24:23 +0330

    update gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,6 @@
-require("dotenv").config();
-
+require("dotenv").config({
+  path: `.env`,
+});
 const { useGatsbyConfig } = require("gatsby-plugin-ts-config");
 
 module.exports = useGatsbyConfig("./gatsby/gatsby-config");
